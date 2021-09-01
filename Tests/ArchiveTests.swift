5b874a7b75f102af62fde873a2c27f0a3d1bb0f3
vaux <zero.griffin@gmail.com>
2021-09-01 18:22:28 +0200

    Remove archive tests

--- Tests/ArchiveTests.swift
+++ Tests/ArchiveTests.swift
@@ -9,25 +9,9 @@ final class ArchiveTests: XCTestCase {
         archive = .new
     }
     
-    func testCapacity() async {
-        XCTAssertEqual(1, archive.capacity)
-        archive.capacity = 100
-        archive = await Archive.prototype(data: archive.compressed)
-        XCTAssertEqual(100, archive.capacity)
-    }
-    
     func testAvailable() {
         XCTAssertTrue(archive.available)
         archive.secrets = [.new]
         XCTAssertFalse(archive.available)
-    }
-    
-    func testSecrets() async {
-        XCTAssertTrue(archive.secrets.isEmpty)
-        archive.secrets.append(.new
-                                .with(payload: "hello"))
-        archive = await Archive.prototype(data: archive.compressed)
-        XCTAssertEqual(1, archive.secrets.count)
-        XCTAssertEqual("hello", archive.secrets.first?.payload)
     }
 }
