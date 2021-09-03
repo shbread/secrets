31d98ac95d5587e8ea98c5b668ed709ca3e33fd5
vaux <zero.griffin@gmail.com>
2021-09-03 17:38:19 +0200

    Add onboarded

--- Sources/Defaults.swift
+++ Sources/Defaults.swift
@@ -3,7 +3,8 @@ import Foundation
 public enum Defaults: String {
     case
     _rated,
-    _created
+    _created,
+    _onboarded
 
     public static var rated: Bool {
         get { self[._rated] as? Bool ?? false }
@@ -13,6 +14,11 @@ public enum Defaults: String {
     public static var created: Date? {
         get { self[._created] as? Date }
         set { self[._created] = newValue }
+    }
+    
+    public static var onboarded: Bool {
+        get { self[._onboarded] as? Bool ?? false }
+        set { self[._onboarded] = newValue }
     }
     
     private static subscript(_ key: Self) -> Any? {
