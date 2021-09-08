a188a6495b79bd907a830a87d687e3c65c833fcc
vaux <zero.griffin@gmail.com>
2021-09-08 09:58:55 +0200

    Add additional default settings

--- Sources/Defaults.swift
+++ Sources/Defaults.swift
@@ -5,7 +5,10 @@ public enum Defaults: String {
     _rated,
     _created,
     _onboarded,
-    _authenticate
+    _authenticate,
+    _spell,
+    _correction,
+    _tools
 
     public static var rated: Bool {
         get { self[._rated] as? Bool ?? false }
@@ -25,6 +28,21 @@ public enum Defaults: String {
     public static var authenticate: Bool {
         get { self[._authenticate] as? Bool ?? false }
         set { self[._authenticate] = newValue }
+    }
+    
+    public static var spell: Bool {
+        get { self[._spell] as? Bool ?? true }
+        set { self[._spell] = newValue }
+    }
+    
+    public static var correction: Bool {
+        get { self[._correction] as? Bool ?? false }
+        set { self[._correction] = newValue }
+    }
+    
+    public static var tools: Bool {
+        get { self[._tools] as? Bool ?? true }
+        set { self[._tools] = newValue }
     }
     
     private static subscript(_ key: Self) -> Any? {
